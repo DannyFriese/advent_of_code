87e43269f7c21e60a3ae8a919664999d41769827
DannyFriese <dfriese@babbel.com>
2021-12-02 12:22:04 +0100

    🎄🎅🏼 t to_i

--- 01_advent_depth.rb
+++ 01_advent_depth.rb
@@ -1,12 +1,11 @@
 require_relative 'puzzle_input/01_input'
 
 # Task 1
-
 positions = POSITIONS.map(&:to_i)
 counter_sum = 0
 
 (1..1999).each do |e|
-  if positions[e].to_i > positions[e-1].to_i
+  if positions[e] > positions[e-1]
     counter_sum += 1
     end
 end
@@ -18,8 +17,8 @@ counter_sum = 0
 previous_sum = 0
 current_sum = 0
 
-(1..1999).each do |e|
-  current_sum = positions[e].to_i + positions[e+1].to_i + positions[e+2].to_i
+(1..1997).each do |e|
+  current_sum = positions[e] + positions[e+1] + positions[e+2]
   if current_sum > previous_sum
     counter_sum += 1
   end
